bb2a626bd234bad6312adf9fd063fb1a1b83acfc
Robert Ziengs <r.r.c.ziengs@gmail.com>
2018-07-20 18:37:35 +0200

    Add project files.

---
+++ DndCharacterGenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DndCharacterGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
