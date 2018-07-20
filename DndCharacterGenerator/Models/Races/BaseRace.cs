b7212138905d8062541d72253fe88da958c7dc3a
Robert Ziengs <r.r.c.ziengs@gmail.com>
2018-07-20 19:01:26 +0200

    init

---
+++ DndCharacterGenerator/Models/Races/BaseRace.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DndCharacterGenerator.Races
+{
+    class BaseRace
+    {
+    }
+}
--- DndCharacterGenerator/Program.cs
+++ DndCharacterGenerator/Program.cs
@@ -4,9 +4,23 @@ namespace DndCharacterGenerator
 {
     class Program
     {
+        private static string[] Classes = new string[] { "Bard", "Rogue", "Ranger", "Paladin", "Fighter"};
+        private static int ClassIndex;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Choose your class");
+            Console.WriteLine();
+            for (int i = 1; i < Classes.Length + 1; i++)
+            {
+                Console.WriteLine("Type " + i + " for " + Classes[i - 1]);
+            }
+            Console.WriteLine();
+            var command = Console.ReadLine();
+            ClassIndex = int.Parse(command) - 1;
+            Console.WriteLine();
+            Console.WriteLine("You have chosen: " + Classes[ClassIndex]);
+            command = Console.ReadLine();
         }
     }
 }
